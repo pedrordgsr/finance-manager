6d5ecbd91b9b2596d43c9c15451088df4dd47365
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2026-03-30 17:46:21 -0300

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./lib/i18n.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "http://localhost:4000",
+    "http://127.0.0.1:4000",
+    "http://100.103.246.119:4000",
+  ],
 };
 
 export default withNextIntl(nextConfig);
